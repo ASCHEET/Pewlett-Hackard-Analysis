7e6570b9cffa6fc0d4ce7554af5eed9ac7a1a726
ASCHEET <88494881+ASCHEET@users.noreply.github.com>
2021-10-06 14:25:56 -0500

    7.3.2

--- Module_7/queries.sql
+++ Module_7/queries.sql
@@ -1,14 +1,12 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE Departments (
+CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
 
-DROP TABLE Employees
-
-CREATE TABLE Employees (
+CREATE TABLE employees (
     emp_no INT NOT NULL,
     birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
@@ -37,21 +35,62 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Dept_emp (
-	dept_no VARCHAR NOT NULL,
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
-CREATE TABLE Titles (
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN  (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
-);
+
+SELECT * FROM departments
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+	
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Count Retirement Eligibility
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+--Export to separate table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+SELECT * FROM retirement_info;
--- Module_7/schema.sql
+++ Module_7/schema.sql
@@ -1,14 +1,12 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE Departments (
+CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
 
-DROP TABLE Employees
-
-CREATE TABLE Employees (
+CREATE TABLE employees (
     emp_no INT NOT NULL,
     birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
@@ -37,16 +35,16 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Dept_emp (
-	dept_no VARCHAR NOT NULL,
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
-CREATE TABLE Titles (
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
