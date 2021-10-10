d594435211d753ae18d732871c977ea403646bb7
ASCHEET <88494881+ASCHEET@users.noreply.github.com>
2021-10-10 10:11:09 -0500

    Challenge_updated_commit

--- Data/Employee_Database_Challenge.sql
+++ Data/Employee_Database_Challenge.sql
@@ -66,6 +66,33 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
 
+-- Count Total Employees
+SELECT COUNT(first_name)
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01';
 
+--QUERY ONE Total current employee count by department number
+SELECT COUNT(e.emp_no), d.dept_name
+	FROM employees as e
+INNER JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+	ON de.dept_no = d.dept_no
+WHERE de.to_date = '9999-01-01'
+GROUP BY d.dept_name
+ORDER BY d.dept_name;
+
+--QUERY 2 Retirement eligible by dept
+SELECT COUNT(*), d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+JOIN departments as d
+	ON de.dept_no = d.dept_no
+WHERE de.to_date = '9999-01-01' AND (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+GROUP BY d.dept_name
+ORDER BY d.dept_name;
 
 
